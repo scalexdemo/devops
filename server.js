473bfef0633b0afcd064259e2ad43d27ca04b612
scalexdemo <97677497+scalexdemo@users.noreply.github.com>
2022-01-14 23:23:48 +0530

    aa

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Hemant, Wellcome to devops project..\n');
+  res.send('Hello Hemant, wellcome to devops project..\n');
 });
 
 app.listen(PORT);

e70b96133c0ee84b786506e91a658e1f59ef8132
scalexdemo <97677497+scalexdemo@users.noreply.github.com>
2022-01-14 22:29:11 +0530

    l

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Hemant, wellcome to kubernetes.\n');
+  res.send('Hello Hemant, wellcome to k8s.\n');
 });
 
 app.listen(PORT);
